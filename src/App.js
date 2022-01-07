5cd18e75f63afa02b2bfcab206fce1e192005192
Rostyk Podolets <ros.podolets@gmail.com>
2022-01-07 14:50:27 +0200

    searchbar markup created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+import React, { Component } from "react";
+
+import Searchbar from "./components/Searchbar/Searchbar";
+
+class App extends Component {
+  render() {
+    return <Searchbar />;
+  }
 }
 
 export default App;
