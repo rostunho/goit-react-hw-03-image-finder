e42996dead13ef40ab5929aef1f026d62186a22f
Rostyk Podolets <ros.podolets@gmail.com>
2022-01-07 15:37:18 +0200

    search query delivered to app state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 import Searchbar from "./components/Searchbar/Searchbar";
 
 class App extends Component {
+  state = {
+    searchQuery: "",
+  };
+
+  onSearchQuery = (query) => {
+    this.setState({ searchQuery: query });
+  };
+
   render() {
-    return <Searchbar />;
+    return <Searchbar onSubmit={this.onSearchQuery} />;
   }
 }
 
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
 
 class Searchbar extends Component {
+  state = {
+    query: "",
+  };
+
+  handleInput = (event) => {
+    // console.log(event.target.value);
+    this.setState({ query: event.target.value });
+  };
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+
+    // console.log(this.state.query);
+
+    this.props.onSubmit(this.state.query);
+  };
+
   render() {
+    // console.log(this.props);
+
     return (
-      <header class="searchbar">
-        <form class="form">
+      <header className="searchbar">
+        <form className="form" onSubmit={this.onSubmitForm}>
           <input
-            class="input"
+            className="input"
             type="text"
-            autocomplete="off"
-            autofocus
+            autoComplete="off"
+            autoFocus
             placeholder="Search images and photos"
+            onChange={this.handleInput}
           />
 
-          <button type="submit" class="button">
-            <span class="button-label">Search</span>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
           </button>
         </form>
       </header>
